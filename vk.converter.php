23578992dbd8e9172109d071fd876497c4b87906
Kirill Berezin <enelar@exsul.net>
2016-08-04 09:16:12 +0300

    Resolve users in chatroom. Compatibility with v0.3

--- vk.converter.php
+++ vk.converter.php
@@ -11,14 +11,37 @@ class converter
     return $ret;
   }
 
+  // Refactor
   public function thread($fetched_thread)
+  {
+    if (isset($fetched_thread->chat_id))
+      return $this->thread_multiuser($fetched_thread);
+    return $this->thread_singleuser($fetched_thread);
+  }
+
+  private function thread_singleuser($fetched_thread)
   {
     $thread = new \synchrotalk\connector\objects\thread($fetched_thread->user_id);
 
     $thread->title = '';
     $thread->is_muted = false;
 
-    $thread->users = [];
+    $thread->users = $this->bunchof_owners([$fetched_thread->user_id]);
+    $thread->owner = $this->owner($fetched_thread->user_id);
+    $thread->last_messages = $this->bunchof_messages($fetched_thread->items);
+
+    return $thread;
+  }
+
+  private function thread_multiuser($fetched_thread)
+  {
+    $thread = new \synchrotalk\connector\objects\thread('#'.$fetched_thread->chat_id);
+
+    $thread->title = $fetched_thread->title;
+    $thread->is_muted = false;
+
+    $thread->users = $this->bunchof_owners($fetched_thread->chat_active);
+    $thread->owner = $this->owner($fetched_thread->admin_id);
     $thread->last_messages = $this->bunchof_messages($fetched_thread->items);
 
     return $thread;
@@ -39,9 +62,23 @@ class converter
     $message->attachements = [];
     $message->text = $item->body;
 
-    $message->owner = new \synchrotalk\connector\objects\owner($item->from_id);
+    $message->owner = $this->owner($item->from_id);
     $message->created = $item->date;
 
     return $message;
+  }
+
+  public function bunchof_owners($array_of_owners)
+  {
+    $ret = [];
+    foreach ($array_of_owners as $fetched_owner)
+      $ret[] = $this->owner($fetched_owner);
+
+    return $ret;
+  }
+
+  public function owner($fetched_owner)
+  {
+    return new \synchrotalk\connector\objects\owner($fetched_owner);
   }
 }
